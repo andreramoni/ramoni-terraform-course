fed78ab1dc705c96d0056da96efe8e962b0c4fc7
Andre Guimaraes <andre.ramoni@gmail.com>
2024-03-04 13:27:21 -0300

    .

---
+++ examples/102c/meta.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "my_null1" {
+  count = 6
+}
+
+output "my_null1" { value = null_resource.my_null1.*.id }
+
+
+resource "null_resource" "my_null0" {
+  depends_on = [ null_resource.my_null0 ]
+
+  #lifecycle { create_before_destroy = true }
+
+  ## This is an argument that can be used to trigger resource replacement:
+  #triggers = { timestamp = timestamp() }
+}
+
